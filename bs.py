54c7738456c6debb27a65cffbe394cc2f1b53671
Glogzi <Krzysiakjanek@gmail.com>
2024-02-18 13:31:09 +0100

    splat code into 2 files, added option to set board size and amount of ships

---
+++ bs.py
@@ -0,0 +1,124 @@
+import numpy as np
+import os
+
+
+def set_ships_num(br_size):
+    inp = 2
+    while True:
+        noterr = True
+        try:
+            inp = int(input(">"))
+        except:
+            noterr = False
+        if inp > br_size:
+            noterr = False
+        if inp < 1:
+            noterr = False
+        if not noterr:
+            print("incorrect number of ships, number cannot be larger or equal to board size, and smaller than 1")
+            continue
+        break
+    return inp
+
+
+def set_board_size():
+    inp = 5
+    while True:
+        noterr = True
+        try:
+            inp = int(input(">"))
+        except:
+            noterr = False
+        if inp > 9:
+            noterr = False
+        if inp < 2:
+            noterr = False
+        if not noterr:
+            print("incorrect board size, please enter size not larger than 9 and not smaller than 2")
+            continue
+        break
+    return inp
+
+
+def enter_to_clear():
+    input(">")
+    os.system("cls||clear")
+
+
+def print_board(board):
+    for line in board:
+        for column in line:
+            print(column, end="")
+        print("")
+
+
+def set_ships(ships, br_size):
+    print("set ship by writing coordinates where you want to place it")
+    print("example: \"2 3\". this will place ship on x=2 y=3 tile")
+    while True:
+        board = create_board(br_size)
+        print_board(board)
+        error = False
+        for i in range(ships):
+            player_input = input(">")
+            input_array = player_input.split(" ")
+            if "0" in input_array:
+                error = True
+            else:
+                try:
+                    if board[int(input_array[1]), int(input_array[0])] == "|":
+                        error = True
+                    else:
+                        board[int(input_array[1]), int(input_array[0])] = "|"
+                except:
+                    error = True
+            print_board(board)
+        if not error:
+            return board
+        print("error, set ships again, now correctly")
+
+
+def create_board(br_size):
+    board = np.zeros((br_size+1, br_size+1), dtype=str)
+    board[:,] = "#"
+    for i in range(len(board)):
+        board[0, i] = str(i)
+    for i in range(len(board)):
+        board[i, 0] = str(i)
+    board[0, 0] = " "
+    return board
+
+
+def check_win(board, ships):
+    count = 0
+    for arr in board:
+        for tile in arr:
+            if tile == "X":
+                count += 1
+    if count == ships:
+        return True
+    return False
+
+
+def next_round(player, board, enemy_board):
+    print(f"{player} round, click enter to continue")
+    enter_to_clear()
+    print("that's your board")
+    print_board(board)
+    print("where you wanna shoot? (write x and y  cords like when setting ships)")
+    while True:
+        error = False
+        player_input = input(">")
+        input_array = player_input.split(" ")
+        if "0" in input_array:
+            error = True
+        else:
+            try:
+                if enemy_board[int(input_array[1]), int(input_array[0])] == "|":
+                    print("hit and sink (tell it to the 2nd player)")
+                    enemy_board[int(input_array[1]), int(input_array[0])] = "X"
+            except:
+                error = True
+        if not error:
+            return board
+        print("error, set tiles again, now correctly")
--- main.py
+++ main.py
@@ -1,112 +1,29 @@
-import numpy as np
-import os
-
-
-def enter_to_clear():
-    input(">")
-    os.system("cls||clear")
-
-
-def print_board(board):
-    for line in board:
-        for column in line:
-            print(column, end="")
-        print("")
-
-
-def set_ships(ships, size):
-    print("set ship by writing coordinates where you want to place it")
-    print("example: \"2 3\". this will place ship on x=2 y=3 tile")
-    while True:
-        board = create_board(size)
-        print_board(board)
-        error = False
-        for i in range(ships):
-            player_input = input(">")
-            input_array = player_input.split(" ")
-            if "0" in input_array:
-                error = True
-            else:
-                try:
-                    if board[int(input_array[1]), int(input_array[0])] == "|":
-                        error = True
-                    else:
-                        board[int(input_array[1]), int(input_array[0])] = "|"
-                except:
-                    error = True
-            print_board(board)
-        if not error:
-            return board
-        print("error, set tiles again, now correctly")
-
-
-def create_board(size):
-    board = np.zeros((size+1, size+1), dtype=str)
-    board[:,] = "#"
-    for i in range(len(board)):
-        board[0, i] = str(i)
-    for i in range(len(board)):
-        board[i, 0] = str(i)
-    board[0, 0] = " "
-    return board
-
-
-def check_win(board, ships):
-    count = 0
-    for arr in board:
-        for tile in arr:
-            if tile == "X":
-                count += 1
-    if count == ships:
-        return True
-    return False
-
-
-def next_round(player, board, enemy_board):
-    print(f"{player} round, click enter to continue")
-    enter_to_clear()
-    print("that's your board")
-    print_board(board)
-    print("where you wanna shoot? (write x and y  cords like when setting ships)")
-    while True:
-        error = False
-        player_input = input(">")
-        input_array = player_input.split(" ")
-        if "0" in input_array:
-            error = True
-        else:
-            try:
-                if enemy_board[int(input_array[1]), int(input_array[0])] == "|":
-                    print("hit and sink (tell it to the 2nd player)")
-                    enemy_board[int(input_array[1]), int(input_array[0])] = "X"
-            except:
-                error = True
-        if not error:
-            return board
-        print("error, set tiles again, now correctly")
+import bs
 
 
 if __name__ == '__main__':
+    print("set board size:")
+    size = bs.set_board_size()
+    print("set amount of ships")
+    ships_num = bs.set_ships_num(size)
     print("player 1 now will set ships")
-    b1 = set_ships(2, 5)
+    b1 = bs.set_ships(ships_num, size)
     print("that's your board, do not show it to player 2")
-    print_board(b1)
     print("to let player 2 set ships click enter")
-    enter_to_clear()
+    bs.enter_to_clear()
     print("player 2 now will set ships")
-    b2 = set_ships(2, 5)
+    b2 = bs.set_ships(ships_num, size)
     print("that's your board, do not show it to player 1")
-    print_board(b2)
     print("click enter to continue")
-    enter_to_clear()
+    bs.enter_to_clear()
     winner = ""
     while True:
-        next_round("player1", b1, b2)
-        if check_win(b2, 2):
+        bs.next_round("player1", b1, b2)
+        if bs.check_win(b2, ships_num):
             winner = "player1"
             break
-        next_round("player2", b2, b1)
-        if check_win(b1, 2):
+        bs.next_round("player2", b2, b1)
+        if bs.check_win(b1, ships_num):
             winner = "player2"
             break
     print(f"player {winner} won!")
